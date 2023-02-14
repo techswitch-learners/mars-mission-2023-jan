62356438cd3be644ad926f64dd566c5a5356ddd3
Hannah Hukins <hannah.hukins@gmail.com>
2023-02-14 14:59:39 +0000

    Mobile changes started

--- src/components/navigation/Header.tsx
+++ src/components/navigation/Header.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Navbar } from "./Navbar";
+import { Navbarmobile } from "./Navbarmobile";
+
 import "./Header.scss";
 
 export const Header: React.FunctionComponent = () => {
   return (
     <header>
       <Navbar />
+      <Navbarmobile />
     </header>
   );
 };
