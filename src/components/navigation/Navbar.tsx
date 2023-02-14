4e7a57d4e2d1f7149a04a0822002c2e0562a8b7e
Michael <minimike100@gmail.com>
2023-02-14 14:06:51 +0000

    Desktop Navbar format skeleton

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,10 +1,31 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { GiSpaceShuttle } from 'react-icons/gi';
+import { IoMdPlanet, IoIosCamera,IoMdCalendar,IoIosInformationCircleOutline, IoMdMenu } from 'react-icons/io';
+import "./NavBar.scss";
 
 export const Navbar: React.FunctionComponent = () => {
   return <nav>
+    <ul>Logo</ul>
     <ul>
-      <li><Link to="/">Home</Link></li>
+      <li><Link to="/"><IoMdPlanet /><br></br>Home</Link></li>
+    </ul>
+    <ul>
+      <li><Link to="/"><GiSpaceShuttle /><br />Launch</Link></li>
+    </ul>
+    <ul>
+      <li><Link to="/"><IoIosCamera /><br />Photos</Link></li>
+    </ul>
+    <ul>
+      <li><Link to="/"><IoMdCalendar /><br />Birthday</Link></li>
+    </ul>
+    <ul>
+      <li><Link to="/"><IoIosInformationCircleOutline /><br />Info</Link></li>
     </ul>
   </nav>;
 };
+
+
+/*
+
+*/
