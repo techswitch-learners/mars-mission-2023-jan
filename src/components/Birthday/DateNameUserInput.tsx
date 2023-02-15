48a27e78e73ef17413b79d9917af11d67d2c3ef6
Patricia Robles <patriciarobles.tech@gmail.com>
2023-02-15 15:01:49 +0000

    Submission of user birthday input

--- src/components/Birthday/DateNameUserInput.tsx
+++ src/components/Birthday/DateNameUserInput.tsx
@@ -1,24 +1,35 @@
-import React from "react";
+import { useState,FormEvent } from "react";
 
-export function DateNameUserInput(){
-    function handleSubmit(event:any){
-        console.log("hello");
-    }
-
+export function DateNameUserInput(props: {
+    setName:React.Dispatch<React.SetStateAction<string>>,
+    setBirthday:React.Dispatch<React.SetStateAction<string>>,
+	handleSubmit: (event:FormEvent<HTMLFormElement>) => void,
+}){
     return (
-        <form className="date-name-form" onSubmit={(event) => handleSubmit(event)}>
+        <form className="date-name-form" onSubmit={(event) => props.handleSubmit(event)}>
             <label htmlFor="user-name">
 				Name: 
-            
-                <input type="text" name="user-name" id="user-name" placeholder="e.g. John Smith" required></input>
+                <input 
+					type="text" 
+					name="user-name" 
+					id="user-name" 
+					placeholder="e.g. John Smith" 
+					onChange={e=>props.setName(e.target.value)} 
+					required>
+				</input>
 			</label>
             
 			<label htmlFor="user-birthday">
 				Birthday:
-                <input type="date" name="user-birthday" id="user-birthday" required></input>
+                <input 
+                    type="date" 
+                    name="user-birthday" 
+                    id="user-birthday" 
+                    onChange= {e=>props.setBirthday(e.target.value)}
+                    required>
+                </input>
                 <button className="btn info-btn" title="We only have photos after the year of 2012. If your birthday year is earlier that, you could review the photos on the same day of any years after 2012. But you will still get your Mars age!">❔</button>
 			</label>
-           
 
             <button className="btn date-name" type="submit">OK!</button>
         </form>
