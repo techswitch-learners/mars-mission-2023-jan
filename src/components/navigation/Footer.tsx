73230253130a8c94e53b6b7acf8d54da22c357a2
Ying Cheng <yingyingch@yahoo.co.uk>
2023-02-15 11:07:36 +0000

    MM0.3:made stylistic changes

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -3,28 +3,25 @@ import "./Footer.scss";
 import { Route, Routes, Link } from "react-router-dom";
 
 export default function Footer() {
-
-    const [footerRight, setFooterRight] = useState(true);
-
-
-    return (<div className='footerSection'>
-        <div className='footerLeft'>
-            <h2>Useful Links</h2>
-            <Link to="#">Terms of Use</Link><br />
-            <Link to="" >Legal</Link><br />
-            <a href="https://mars.nasa.gov/#red_planet/5">NASA Mars Page</a>
-        </div>
-
-        <div className='footerRight'>
-            <div className='footerRightContent' id={footerRight ? "footerRightAppear" : "footerRightWrapper"}>
-                <h2>Weather on Mars</h2>
-                <div className='footerWeather'>Weather Component Placeholder</div>
+    const [displayWeather, setDisplayWeather] = useState(true);
+    return <div className='footerSection'>
+            <div className='footerLeft'>
+                <h2>Useful Links</h2>
+                <Link to="#">Terms of Use</Link><br />
+                <Link to="" >Legal</Link><br />
+                <a href="https://mars.nasa.gov/#red_planet/5">NASA Mars Page</a>
             </div>
-            <div className='footerRightContent' id={footerRight ? "footerRightWrapper" : "footerRightAppear"}>
-                <h2>Fun Fact</h2>
-                <div className='footerFact'>Fun Fact Placeholder</div>
+
+            <div className='footerRight'>
+                <div className='footerRightContent' id={displayWeather ? "footerRightAppear" : "footerRightWrapper"}>
+                    <h2>Weather on Mars</h2>
+                    <div className='footerWeather'>Weather Component Placeholder</div>
+                </div>
+                <div className='footerRightContent' id={displayWeather ? "footerRightWrapper" : "footerRightAppear"}>
+                    <h2>Fun Fact</h2>
+                    <div className='footerFact'>Fun Fact Placeholder</div>
+                </div>
+                <button className="footerButton" onClick={() => setDisplayWeather(!displayWeather)}>{displayWeather ? "Check Fun Facts" : "Check Weather"}</button>
             </div>
-            <button className="footerButton" onClick={() => setFooterRight(!footerRight)}>{footerRight ? "Check Fun Facts" : "Check Weather"}</button>
         </div>
-    </div>)
 }
