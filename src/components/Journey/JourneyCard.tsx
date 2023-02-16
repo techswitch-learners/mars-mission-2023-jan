8b621da1b084a265f1199a18c03b0a80a362e0df
Patricia Robles <patriciarobles.tech@gmail.com>
2023-02-16 14:00:57 +0000

    cahnges to content function - work in progress

--- src/components/Journey/JourneyCard.tsx
+++ src/components/Journey/JourneyCard.tsx
@@ -1,10 +1,11 @@
 import "./Journey.scss";
+import { journeyContent } from "./journeyContent";
 
-export function JourneyCard(){
+export function JourneyCard(props:{text:string,imgUrl:string}){
     return (
         <section className="journey-card">
-            <img className="journey-card-image"></img>
-            <article className="journey-card-text"></article>     
+            <img className="journey-card-image" src=""></img>
+            <article className="journey-card-text">{}</article>     
         </section>
     )
 }
