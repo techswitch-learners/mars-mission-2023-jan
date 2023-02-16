f21b16c4dfe0546fc1d51a57ceaa0ede5449c872
Bindiya Kejdiwal <Bindiya.Kejdiwal@softwire.com>
2023-02-16 11:27:47 +0000

    Added correct routes for components.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import "./App.scss";
 import { Header } from "./navigation/Header";
 import { Route, Routes } from "react-router-dom";
+import { Home } from "./Home/Home";
+import { Journey } from "./Journey/Journey";
+import { MarsViewer } from "./marsPhotoViewer/marsViewer";
+import { Birthday } from "./Birthday/Birthday";
 import Footer from "./navigation/Footer";
-import {Home} from './Home/Home'
-import {Journey} from './Journey/Journey'
 
 const App: React.FunctionComponent = () => {
-
   return (
     <>
       <Header />
       <Routes>
-      {/* under construction */}
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/journey" element={<Journey/>} />
-        <Route path="/mars-viewer" element={<div>MarsViewer</div>} />
-        <Route path="/birthday" element={<div>Birthday</div>} />
+        <Route path="/mars-viewer" element={<MarsViewer/>} />
+        <Route path="/birthday" element={<Birthday/>} />
         <Route path="/info" element={<div>Info</div>} />
       </Routes>
-      <footer><Footer /></footer>
+      <Footer />
     </>
   );
 };
