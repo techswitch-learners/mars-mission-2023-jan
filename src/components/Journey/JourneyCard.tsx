4c4b1b8446092ddcef412bab4a6552aa7ad16ffd
Patricia Robles <patriciarobles.tech@gmail.com>
2023-02-16 14:22:17 +0000

    create function to return content array

--- src/components/Journey/JourneyCard.tsx
+++ src/components/Journey/JourneyCard.tsx
@@ -1,11 +1,14 @@
 import "./Journey.scss";
 import { journeyContent } from "./journeyContent";
 
-export function JourneyCard(props:{text:string,imgUrl:string}){
+export function JourneyCard(){
+    const content = journeyContent()
+
     return (
         <section className="journey-card">
-            <img className="journey-card-image" src=""></img>
-            <article className="journey-card-text">{}</article>     
+            {/* will change src and text in another branch */}
+            <img className="journey-card-image" src={content[0].img}></img>
+            <article className="journey-card-text">{content[0].text}</article>     
         </section>
     )
 }
