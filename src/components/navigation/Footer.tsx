9f9e5f22343cbecf2b5799e6e7193c7b15ea4a25
Michael <minimike100@gmail.com>
2023-02-17 13:53:45 +0000

    Formatting

--- src/components/navigation/Footer.tsx
+++ src/components/navigation/Footer.tsx
@@ -1,24 +1,23 @@
 import { useState } from "react";
 import "./Footer.scss";
-import {AstroImage} from "../AstroImage";
+import { AstroImage } from "../AstroImage";
 import { Facts } from "../Facts/Facts";
 import { Link } from "react-router-dom";
 
 export default function Footer() {
     const [displayWeather, setDisplayWeather] = useState(true);
     return <div className='footerSection'>
-            <div className='footerLeft'> 
-                <h2>Useful Links</h2>
-                <Link to="#">Terms of Use</Link><br />
-                <Link to="" >Legal</Link><br />
-                <a href="https://mars.nasa.gov/#red_planet/5">NASA Mars Page</a>
-            </div>
+        <div className='footerLeft'>
+            <h2>Useful Links</h2>
+            <Link to="#">Terms of Use</Link><br />
+            <Link to="" >Legal</Link><br />
+            <a href="https://mars.nasa.gov/#red_planet/5">NASA Mars Page</a>
+        </div>
 
-            <div className='footerRight'>
-                <div  id={displayWeather ? "footerRightAppear" : "footerRightWrapper"}>
-                    <h2 className='footerRightTitle'>Today's Astro Image</h2>
-                    <div className='footerRightContent'><AstroImage /></div>
-                </div>
+        <div className='footerRight'>
+            <div id={displayWeather ? "footerRightAppear" : "footerRightWrapper"}>
+                <h2 className='footerRightTitle'>Today's Astro Image</h2>
+                <div className='footerRightContent'><AstroImage /></div>
             </div>
             <div className='footerRightContent' id={displayWeather ? "footerRightWrapper" : "footerRightAppear"}>
                 <h2>Fun Fact</h2>
