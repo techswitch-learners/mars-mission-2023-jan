7e50482cba1832c1764a5e33132a98ea7bbd4aa9
Ying Cheng <yingyingch@yahoo.co.uk>
2023-02-17 11:28:32 +0000

    MM2.2:made review changes

--- src/components/marsPhotoViewer/ImageSelector.tsx
+++ src/components/marsPhotoViewer/ImageSelector.tsx
@@ -8,20 +8,26 @@ export function ImageSelector(props: {
     rover: string,
     camera: string,
 }) {
-    const [imgUrl, setImgUrl] = useState<imageApiModel[]>();
+    const [apiImages, setApiImages] = useState<imageApiModel[]>();
 
     useEffect(() => {
         getImagesFromApi(props.date, props.rover, props.camera)
-            .then(data => setImgUrl(data))
-            .then(() => { if (imgUrl) props.setState(imgUrl[0].img_src) });
-    }, []);
+            .then(data => setApiImages(data))
+            .then(() => { if (apiImages) props.setState(apiImages[0].img_src) });
+    }, [props.date, props.rover, props.camera]);
 
-    const urls = imgUrl ? imgUrl.slice(0, Math.min(imgUrl.length, 12)).map(url => <img src={url.img_src} key={url.id} className="individualImage" alt='Mars image'
-        onClick={() => props.setState(url.img_src)} />) : <p>No Image to Display</p>;
+    const images = apiImages
+        ? apiImages.slice(0, Math.min(apiImages.length, 12))
+            .map(data => <img src={data.img_src}
+                key={data.id}
+                className="individualImage"
+                alt='Mars image'
+                onClick={() => props.setState(data.img_src)} />)
+        : <p>No Image to Display</p>;
 
     return <div>
         <h4>Select a Photo</h4>
         <h5>Showing for the {props.rover} rover's {props.camera} on {props.date}.</h5>
-        <div className="imageSelector">{urls}</div>
+        <div className="imageSelector">{images}</div>
     </div>
 }
